647a5b57e018ba8e0bc8d26e2117a27ba44afb71
Bongani Qunta <magine20@gmail.com>
2020-03-15 11:20:46 +0200

    added login page(view), register page(view) and admin encryption class

---
+++ UI/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Controllers
+{
+    [RoutePrefix("login")]
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+        [Route("register")]
+        [HttpGet]
+        public ActionResult register()
+        {
+            return View();
+        }
+
+    }
+}
